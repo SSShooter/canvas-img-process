d7a7ba0f193f2f6372cf60e02ba11d3c8ff2fa31
ssshooter <472356884@qq.com>
2018-03-28 23:53:51 +0800

    转换图片功能思考中

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,62 @@
 <template>
   <div id="app">
-    <aside>
-      <input type="file" @change="getCustomImage"><br>
-      <button @click="bOrA = !bOrA">{{bOrA?'before':'after'}}</button>
-      <button @click="generateAsciiArt">ascii art</button>
-      <div class="matrix">
-        <input type="number" v-model="custom[0]">
-        <input type="number" v-model="custom[1]">
-        <input type="number" v-model="custom[2]">
-        <input type="number" v-model="custom[3]">
-        <input type="number" v-model="custom[4]">
-        <input type="number" v-model="custom[5]">
-        <input type="number" v-model="custom[6]">
-        <input type="number" v-model="custom[7]">
-        <input type="number" v-model="custom[8]">
+    <div class="system">
+      <aside>
+        <input type="file"
+          @change="getCustomImage"><br>
+        <button @click="bOrA = !bOrA">{{bOrA?'before':'after'}}</button>
+        <button @click="generateAsciiArt">ascii art</button>
+        <div class="matrix">
+          <input type="number"
+            v-model="custom[0]">
+          <input type="number"
+            v-model="custom[1]">
+          <input type="number"
+            v-model="custom[2]">
+          <input type="number"
+            v-model="custom[3]">
+          <input type="number"
+            v-model="custom[4]">
+          <input type="number"
+            v-model="custom[5]">
+          <input type="number"
+            v-model="custom[6]">
+          <input type="number"
+            v-model="custom[7]">
+          <input type="number"
+            v-model="custom[8]">
+        </div>
+        <div>
+          <button @click="custom = [0,0,0,0,1,0,0,0,0]">重置</button>
+          <button @click="custom = [0.0625, 0.125, 0.0625, 0.125, 0.25, 0.125, 0.0625, 0.125, 0.0625]">模糊</button>
+          <button @click="custom = [0, -1, 0, -1, 5, -1, 0, -1, 0]">锐化</button>
+          <button @click="custom = [-2, -1, 0, -1, 1, 1, 0, 1, 2]">浮雕</button>
+          <button @click="custom = [0, 0, 0, -1, 1, 0, 0, 0, 0]">边缘增强</button>
+          <button @click="custom =  [0, 1, 0, 1, -4, 1, 0, 0, 0]">边缘检测</button>
+        </div>
+      </aside>
+      <div class="canvas-display">
+        <canvas id="input"
+          :width="width"
+          :height="height">
+        </canvas>
+        <canvas v-show="bOrA"
+          id="output"
+          :width="width"
+          :height="height">
+        </canvas>
       </div>
-      <div>
-        <button @click="custom = [0,0,0,0,1,0,0,0,0]">重置</button>
-        <button @click="custom = [0.0625, 0.125, 0.0625, 0.125, 0.25, 0.125, 0.0625, 0.125, 0.0625]">模糊</button>
-        <button @click="custom = [0, -1, 0, -1, 5, -1, 0, -1, 0]">锐化</button>
-        <button @click="custom = [-2, -1, 0, -1, 1, 1, 0, 1, 2]">浮雕</button>
-        <button @click="custom = [0, 0, 0, -1, 1, 0, 0, 0, 0]">边缘增强</button>
-        <button @click="custom =  [0, 1, 0, 1, -4, 1, 0, 0, 0]">边缘检测</button>
-      </div>
-    </aside>
-    <div class="canvas-display">
-      <canvas id="input" :width="width" :height="height">
-      </canvas>
-      <canvas v-show="bOrA" id="output" :width="width" :height="height">
-      </canvas>
     </div>
-    <canvas id="mini" :width="width/4" :height="height/4">
+    <canvas id="mini"
+      :width="width/4"
+      :height="height/4">
     </canvas>
-    <pre><div class="art" v-for="line in asciiArray"><span v-for="bit in line">{{bit}}</span></div></pre>
+    <pre id="ascii-art"><div class="art" v-for="line in asciiArray"><span v-for="bit in line">{{bit}}</span></div></pre>
   </div>
 </template>
 
 <script>
+import html2canvas from 'html2canvas'
 export default {
   name: 'app',
   data() {
@@ -57,6 +77,13 @@ export default {
     }
   },
   methods: {
+    html2Canvas() {
+      var div = document.getElementById('ascii-art')
+      html2canvas(div).then(function(canvas) {
+        document.body.appendChild(canvas);
+      });
+    },
+    canvas2Img() { },
     getCustomImage(e) {
       let vm = this
       let file = e.target.files[0]
@@ -99,7 +126,7 @@ export default {
                 m[6] * iD[i + w * 4 - 4] +
                 m[7] * iD[i + w * 4] +
                 m[8] * iD[i + w * 4 + 4]) /
-                divisor
+              divisor
           }
           oD[(y * w + x) * 4 + 3] = 255
         }
@@ -109,11 +136,11 @@ export default {
         .getElementById('mini')
         .getContext('2d')
         .drawImage(
-          document.getElementById('output'),
-          0,
-          0,
-          this.width / 4,
-          this.height / 4
+        document.getElementById('output'),
+        0,
+        0,
+        this.width / 4,
+        this.height / 4
         )
     },
     generateAsciiArt() {
@@ -142,6 +169,8 @@ export default {
           widthSign = 1
         }
       }
+      vm.$nextTick(() => vm.html2Canvas())
+
     }
   },
   mounted() {
@@ -167,7 +196,7 @@ export default {
 
 <style lang="less">
 @import './common.css';
-#app {
+.system {
   display: flex;
   .canvas-display {
     position: relative;
@@ -177,25 +206,27 @@ export default {
       position: absolute;
     }
   }
-  #mini {
-    position: absolute;
-    top: -1000px;
-  }
-  .art {
-    font-size: 1px;
-    line-height: 3px;
-    font-family: Courier;
-  }
   .matrix {
     display: flex;
     flex-wrap: wrap;
     width: 180px;
   }
-  .matrix > input {
+  .matrix>input {
     box-sizing: border-box;
     height: 60px;
     width: 60px;
     text-align: center;
   }
+}
+
+#mini {
+  position: absolute;
+  top: -1000px;
+}
+
+.art {
+  font-size: 1px;
+  line-height: 7px;
+  font-family: Courier;
 }
 </style>
